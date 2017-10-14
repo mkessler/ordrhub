ed17956bad1a3910eecffdb634a03104e29b0c1b
mkessler <mikaelkessler@gmail.com>
2017-10-14 17:47:43 -0400

    Add sources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014213739) do
+ActiveRecord::Schema.define(version: 20171014214646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20171014213739) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["organization_id"], name: "index_restaurants_on_organization_id", using: :btree
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

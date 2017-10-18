d1cc38d7808d9845e38ff832b62adb1b5aa3f98a
mkessler <mikaelkessler@gmail.com>
2017-10-18 00:25:06 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,17 @@ Source.create(name: 'eat24')
 Source.create(name: 'UberEats')
 
 Organization.create(name: 'Jersey Mike\'s')
-Store.create(
-  organization: Organization.first,
-  name: 'Store #12345'
-)
+
+50.times do
+  Store.create(
+    organization: Organization.first,
+    name: "Store ##{Faker::Number.number(5)}"
+  )
+end
+
 Store.create(name: 'Good Food on Montford')
+Store.create(name: 'ROCKSALT')
+
 User.create(
   email: 'user1@example.com',
   password: 'password'
@@ -31,36 +37,58 @@ Membership.create(
 )
 Association.create(
   store: Store.last,
-  user: User.first
+  user: User.last
 )
 Association.create(
   store: Store.last,
   user: User.last
 )
 
-10.times do
-  Order.create(
-    source: Source.all.sample,
-    store: Store.all.sample,
-    details: {
-      number: "1234567890",
-      customer_name: "Scarlett Johannson",
-      customer_phone: "555-987-6543",
-      delivery_date: "May 29, 2017",
-      delivery_address: "400 5th Ave Room 10234",
-      delivery_time: "10:05 PM",
-      delivery_city: "New York, NY 10018",
-      items: [
-        "1 Focaccia Della Casa $ 4.50",
-        "1 Ensalata Caprese $ 9.00",
-        "1 Pizza Quattro Formaggi $ 14.00"
-      ],
-      subtotal: "$27.50",
-      fee: "$2.00",
-      tax: "$2.62",
-      tip: "$4.82",
-      total: "$36.94",
-      received: "2017-05-29T14:36:36.895831+00:00"
-    }
-  )
+Store.all.each do |store|
+  50.times do
+    am_pm = rand(0..1)
+    order_count = rand(1..5)
+    order_objects = []
+    order_count.times do
+      order_objects << {
+        item: Faker::Food.dish,
+        amount: rand(2.99...18.99).round(2),
+        count: rand(1..3)
+      }
+    end
+    subtotal = order_objects.sum { |item| item[:amount] * item[:count] }
+    fee = rand(2..4).round(2)
+    tax = (subtotal * 0.07).round(2)
+    tip = (subtotal * 0.18).round(2)
+    total = (subtotal + fee + tax + tip).round(2)
+    received = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+    Order.create(
+      source: Source.all.sample,
+      store: store,
+      details: {
+        number: "#{Faker::Number.number(10)}",
+        customer_name: "#{Faker::Name.name}",
+        customer_phone: "#{Faker::PhoneNumber.phone_number}",
+        delivery_date: received.strftime('%B %-d, %Y'),
+        delivery_address: "#{Faker::Address.street_address}",
+        delivery_time: "#{rand(1..12)}:#{sprintf('%02d',rand(1..12))} #{am_pm == 1 ? 'AM' : 'PM'}",
+        delivery_city: "#{Faker::Address.city}, #{Faker::Address.state_abbr} #{Faker::Address.zip_code}",
+        items: order_objects.map { |order|
+          if order[:count] == 1
+            "#{order[:count]} #{order[:item]} $#{order[:amount]}"
+          else
+            total_amount = (order[:amount] * order[:count]).round(2)
+            "#{order[:count]} #{order[:item]} $#{total_amount} ($#{order[:amount]}/ea)"
+          end
+        },
+        subtotal: "$#{sprintf('%.2f', subtotal)}",
+        fee: "$#{sprintf('%.2f', fee)}",
+        tax: "$#{sprintf('%.2f', tax)}",
+        tip: "$#{sprintf('%.2f', tip)}",
+        total: "$#{sprintf('%.2f', total)}",
+        received: received.to_s
+      }
+    )
+  end
 end

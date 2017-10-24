57082468e27e4276b648fc16bd1de9b05ad9e91d
mkessler <mikaelkessler@gmail.com>
2017-10-24 14:07:07 -0400

    Create orders from zapier

--- app/controllers/zapier_controller.rb
+++ app/controllers/zapier_controller.rb
@@ -2,5 +2,17 @@ class ZapierController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def incoming_order
+    store = Store.first
+    order = store.orders.new({
+      source_id: Source.first.id
+    }.merge(params[:zapier]))
+
+    respond_to do |format|
+      if order.save
+        format.json { render json: { created: true }, status: :created }
+      else
+        format.json { render json: order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end

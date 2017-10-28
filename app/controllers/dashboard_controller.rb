fcd6b275a74476c757b4650fa0e519dec996660d
mkessler <mikaelkessler@gmail.com>
2017-10-27 22:45:37 -0400

    Add pagination at top of stores and orders index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,21 +3,18 @@ class DashboardController < ApplicationController
 
   def index
     if current_user.organizations.count > 1
-      #redirect_to organizations_path
-      redirect_to stores_path
-    elsif current_user.organizations.count > 0
-      # redirect_to organization_path(
-      #   current_user.organizations.first
-      # )
-      redirect_to stores_path
+      redirect_to organizations_path
+    elsif current_user.organizations.present?
+      redirect_to organization_path(
+        current_user.organizations.first
+      )
     elsif current_user.associations.count > 1
       redirect_to stores_path
-    elsif current_user.associations.count > 0
+    elsif current_user.associations.present?
       redirect_to store_path(
         current_user.stores.first
       )
     else
-      #redirect_to home_index_path
       redirect_to new_store_path
     end
   end

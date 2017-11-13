fd36fecbb7fb6a484b436a80309cbb61a29cff95
mkessler <mikaelkessler@gmail.com>
2017-11-12 22:26:45 -0500

    Check if order exists before creating

--- app/controllers/zapier_controller.rb
+++ app/controllers/zapier_controller.rb
@@ -3,27 +3,39 @@ class ZapierController < ApplicationController
   include ZapierHelper
 
   def incoming_grubhub_order
-    store = Store.first
-    email = Nokogiri::HTML(params['zapier']['HtmlDocument'])
-    confirmation_link = email.search('a[href*="orderemails.grubhub.com"]').first.attributes['href'].value
-    order_params = params['zapier'].except('HtmlDocument').merge(
-      { 'confirmation_link' => confirmation_link }
-    )
+    if store = Store.find_by_email(params['sender'])
+      email = Nokogiri::HTML(params['zapier']['HtmlDocument'])
+      confirmation_link = order_confirmation_link(
+        email,
+        params['zapier']['reference']
+      )
+      order_params = params['zapier'].except('HtmlDocument').merge(
+        { 'confirmation_link' => confirmation_link }
+      )
 
-    order = store.orders.new({
-      source_id: order_source_id(params['zapier']['Template']),
-      details: order_params,
-      name: params['zapier']['dropoffName'],
-      number: params['zapier']['reference'],
-      phone_number: params['zapier']['dropoffPhone']
-    })
-
-    respond_to do |format|
-      if order.save
-        format.json { render json: { created: true }, status: :created }
+      if order = Order.find_by_number(params['zapier']['reference'])
+        order.details = order_params
+        order.name = params['zapier']['dropoffName']
+        order.phone_number = params['zapier']['dropoffPhone']
       else
-        format.json { render json: order.errors, status: :unprocessable_entity }
+        order = store.orders.new({
+          source_id: order_source_id(params['zapier']['Template']),
+          details: order_params,
+          name: params['zapier']['dropoffName'],
+          number: params['zapier']['reference'],
+          phone_number: params['zapier']['dropoffPhone']
+        })
       end
+
+      respond_to do |format|
+        if order.save
+          format.json { render json: { created: true }, status: :created }
+        else
+          format.json { render json: order.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      format.json { render json: { store: false }, status: :unprocessable_entity }
     end
   end
 end
--- app/helpers/zapier_helper.rb
+++ app/helpers/zapier_helper.rb
@@ -8,4 +8,14 @@ module ZapierHelper
       nil
     end
   end
+
+  def order_confirmation_link(email, template)
+    formatted_template = template.downcase
+
+    if formatted_template.include?('grubhub')
+      email.search('a[href*="orderemails.grubhub.com"]').first.attributes['href'].value
+    else
+      nil
+    end
+  end
 end

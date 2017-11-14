f61c29911fc0b3cf2f1d8e1b399640d970623170
mkessler <mikaelkessler@gmail.com>
2017-11-14 00:48:29 -0500

    Add in previously set up routes and env configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :leads, only: [:create]
+  resources :organizations
+  resources :stores do
+    post :incoming_order, on: :collection
+    resources :orders, except: [:new, :edit]
+  end
+
+  get 'dashboard/index'
+  get 'marketing/index'
+  get 'privacy_policy', to: "marketing#privacy_policy"
+  post 'zapier/incoming_grubhub_order'
+
+  authenticated :user do
+    root to: redirect('/dashboard/index'), as: :authenticated_root
+  end
+  root to: "marketing#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
